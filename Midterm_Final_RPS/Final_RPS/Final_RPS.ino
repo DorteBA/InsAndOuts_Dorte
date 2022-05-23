f51956e3db05907086ac692886f2a7f48aa4ad94
DorteBA <70597437+DorteBA@users.noreply.github.com>
2022-05-23 09:44:03 -0400

    Done

---
+++ Midterm_Final_RPS/Final_RPS/Final_RPS.ino
@@ -0,0 +1,29 @@
+const int Button1 = 2;
+const int Button2 = 4;
+const int Button3 = 7;
+
+int buttonState1 = 0;
+int buttonState2 = 0;
+int buttonState3 = 0;
+
+void setup() {
+  pinMode(Button1, INPUT);
+  pinMode(Button2, INPUT);
+  pinMode(Button3, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+   buttonState1 = digitalRead(Button1);
+   buttonState2 = digitalRead(Button2);
+   buttonState3 = digitalRead(Button3);
+
+    delay(1000);
+    Serial.write(buttonState1);
+    //Serial.println(buttonState1);
+    //Serial.println(buttonState2);
+    //Serial.println(buttonState3);
+    Serial.write(buttonState2);
+    Serial.write(buttonState3);
+}
